9193d3305798548a829b3c55f6e9f1781b33dfaa
Joshua <joshuajacobs717@gmail.com>
2026-05-18 14:47:31 +0200

    Dependancies

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,7 @@
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   root: __dirname,
   plugins: [react(), tailwindcss()],
