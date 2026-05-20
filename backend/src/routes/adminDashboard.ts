3f4e5fe68a35ef69e01d8860d1ae0cf0abb8b28b
Mihle Aviwe <mihleaviwe6@gmail.com>
2026-05-20 15:31:06 +0200

    chore:/do not merge/pull:We worked ticket:Bottom Part Admin Dashboard. Moreover:The laptop is having component issues and I am pushing for safety reason incase stuff are removed or deleted

---
+++ backend/src/routes/adminDashboard.ts
@@ -0,0 +1,12 @@
+//routes/adminDashboard.routes.ts
+
+import express from 'express';
+import { getRecentActivityController, getCurrentlyOnsiteController } from '../controllers/adminDashboardCon.js';
+
+const router = express.Router();
+
+//Get Recent Activity and Onsite Staff routes
+router.get('/recent-activity', getRecentActivityController);
+router.get('/onsite', getCurrentlyOnsiteController);
+
+export default router;
