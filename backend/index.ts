8804487cf41c039f887a69f38a3f50bb97564d74
Yaqoob-Samsodien <yaqoobsamsodien01gmail.com>
2026-05-21 15:18:18 +0200

    Dashboard - leave/sick requests

--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,24 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import attendanceRoutes from './src/routes/leaveRoutes.js'
+
 dotenv.config()
 const app = express()
 app.use(cors())
 app.use(express.json())
+
+
+app.use((req: any, _res: any, next: any) => {
+  req.auth = {
+    userId:'07cd9434-1b18-4d96-b029-0595b26d067c',
+    role: 'admin',
+    token: 'mock-token'
+  }
+  next()
+})
+
+app.use('/api/leaves', attendanceRoutes)
 
 const port = process.env.PORT || 4321
 
