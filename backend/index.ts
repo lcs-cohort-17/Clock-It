f618932cadcc3a8aeab6118e1b719bf067b27750
Mihle Aviwe <mihleaviwe6@gmail.com>
2026-05-21 10:18:46 +0200

    chore:add recently onsite and recent activity endpoints with pagination

--- backend/index.ts
+++ backend/index.ts
@@ -1,12 +1,15 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import adminDashboardRoutes from './src/routes/adminDashboardRoutes.js'
 dotenv.config()
 const app = express()
 app.use(cors())
 app.use(express.json())
 
 const port = process.env.PORT || 4321
+
+app.use('/api/admin/dashboard', adminDashboardRoutes)
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`)
