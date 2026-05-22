b4b80cd618dd9e6475ac148b02d5c244043ef9af
DanielWatterson <dominicpeck03@gmail.com>
2026-05-22 12:03:11 +0200

    Adjusted files

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,17 +1,9 @@
 import { StrictMode } from 'react'
-<<<<<<< HEAD
-import { createRoot } from 'react-dom/client'
-
-import App from './App'
-import { ThemeProvider } from './context/ThemeContext.tsx'
-import './index.css'
-=======
 import ReactDOM from 'react-dom/client'
->>>>>>> fce12ba4e1b5c5f935464309b32a5d9d75d6c432
+import { ThemeProvider } from './context/ThemeContext.tsx' 
 
 import App from './App.tsx'
 import './index.css'
-
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
