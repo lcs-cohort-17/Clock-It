f0e63cca9a3c739ce04bf88095c26eb97fbeab63
Yaqoob-Samsodien <yaqoobsamsodien01gmail.com>
2026-05-25 09:06:57 +0200

    more fixes

--- backend/src/config/supabase.ts
+++ backend/src/config/supabase.ts
@@ -3,6 +3,6 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const supabaseUrl = process.env.SUPABASE_URL!
-const supabaseKey = process.env.SUPABASE_KEY!
+const supabaseKey = process.env.SUPABASE_SERVICE_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
