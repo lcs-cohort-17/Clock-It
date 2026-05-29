ba1c2801a98e16dba8a5ac3f363ff6ec726b1381
Dominic Peck <46389716+DanielWatterson@users.noreply.github.com>
2026-05-29 08:46:30 +0200

    Delete backend directory

--- backend/src/models/profileDb.ts
+++
@@ -1,6 +0,0 @@
-// import { supabase } from '../config/supabase.js'
-export async function getProfilesDb() {}
-export async function createProfileDb() {}
-export async function updateProfileDb() {}
-export async function deleteProfileDb() {}
-export async function resetPasswordDb() {}
